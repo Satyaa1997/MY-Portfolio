ba944de8c97ae548425c65cbc9a12249595f06b7
Satyaa1997 <satyamrockindia@gmail.com>
2026-04-17 15:07:32 +0530

    link add

--- src/component/Portfolio/Portfolio.jsx
+++ src/component/Portfolio/Portfolio.jsx
@@ -93,6 +93,13 @@ export default function Portfolio() {
             >
               Open Website
             </a>
+             <a
+              href="stupendous-faloodeh-3eef42.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open Website
+            </a>
           </div>
 
           <div className="project-card">
